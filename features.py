4a4ae293f74de54cce7552717596fb43ee1a2c28
Onwuka David <onwukadavid21@gmail.com>
2023-09-07 22:09:49 +0100

    added utility file and moved some functions from features file to utils file.

--- features.py
+++ features.py
@@ -8,36 +8,9 @@ import pprint
 import subprocess
 import re
 import os
+from utils import file_decorator, is_correct_time, split_apps
 
 logger = file_log.get_logger(__name__)
-
-def file_decorator(func):
-    def wrapper(*args, **kwargs):
-        with shelve.open('tasks_details') as task:
-            func(*args, **kwargs, task_file=task)
-    return wrapper
-
-def is_correct_time(time_string):
-    time_regex = re.compile(r'([01]\d|2[0-3]):[0-5]\d')
-    if re.fullmatch(time_regex, time_string) is None:
-        raise ValueError(f'{time_string} is not a proper time format.')
-    time = datetime.datetime.strptime(time_string, '%H:%M').time()
-    return time
-
-def split_apps(app_file):
-    app_file_regex = re.compile(r'.+?\.exe')
-    app_file_match = re.findall(app_file_regex, app_file)
-    logger.debug(app_file_match)
-    if (app_file_match is None):
-        raise ValueError('Please enter a proper application file path.')
-    
-    # Check if the application exists.
-    for match in app_file_match:
-        if not os.path.exists(match):
-            print(f'Failed to save "{match}". It does not exist.\nPlease ensure the right file path was entered.')
-            app_file_match.remove(match)
-    return app_file_match
-
 
 class Task():
     def __init__(self, task_name):
---
+++ utils.py
@@ -0,0 +1,26 @@
+import re
+import shelve
+import datetime
+import file_log
+
+logger = file_log.get_logger(__name__)
+
+def file_decorator(func):
+    def wrapper(*args, **kwargs):
+        with shelve.open('tasks_details') as task:
+            func(*args, **kwargs, task_file=task)
+    return wrapper
+
+def is_correct_time(time_string):
+    time_regex = re.compile(r'([01]\d|2[0-3]):[0-5]\d')
+    if re.fullmatch(time_regex, time_string) is None:
+        raise ValueError(f'{time_string} is not a proper time format.')
+    time = datetime.datetime.strptime(time_string, '%H:%M').time()
+    return time
+
+def split_apps(app_file):
+    app_file_regex = re.compile(r'.+?\.exe')
+    app_file_match = re.findall(app_file_regex, app_file)
+    logger.debug(app_file_match)
+    if (app_file_match is None):
+        raise ValueError('Please enter a proper application file path.')
